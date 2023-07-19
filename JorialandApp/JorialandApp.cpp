749f13cdd57a72c1d0bda3128f5465218839b4a3
HoriaGhionoiuMartinez <horia.ghionoiu@alma-medical.com>
2023-07-19 14:15:12 +0200

    Implemented option to add food.

--- JorialandApp/JorialandApp.cpp
+++ JorialandApp/JorialandApp.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <thread>
 #include <locale>
+#include <sstream>
 
 using namespace std::chrono_literals;
 
@@ -16,14 +17,22 @@ enum class MenuOption
 	EXIT
 };
 
-void displayMenu()
+void CleanScreen()
 {
 	system("cls");
+}
+
+void DisplayMenu()
+{
+	CleanScreen();
 	std::cout << "Elija una opción:\n";
 	std::cout << "1) Añadir comida.\n";
 	std::cout << "2) Ver comidas.\n";
 	std::cout << "3) Salir del programa.\n";
 }
+
+void AddFood()
+{}
 
 int main()
 {
@@ -35,28 +44,53 @@ int main()
 
 	while (true)
 	{
-		displayMenu();
+		DisplayMenu();
 
 		std::getline(std::cin, input);
 		try
 		{
+			// For adding food
+			std::istringstream iss;
+			std::string sTitle, sDate, sPhoto, sDescription, bHungry, eSatisfaction, bBadFood;
+
 			option = static_cast<MenuOption>(std::stoi(input));
 			switch (option)
 			{
 				case MenuOption::ADD_FOOD:
-					std::cout << sNotImplemented << std::endl;
-					std::getline(std::cin, input);
-					break;
+				//std::cout << sNotImplemented << std::endl;
+				CleanScreen();
+				std::cout << "Introduzca los datos separados por comas (Title,Date,Photo,Description,Hungry,Satisfaction,BadFood): ";
+				std::getline(std::cin, input);
+				iss.str(input);
+
+				std::getline(iss, sTitle, ',');
+				std::getline(iss, sDate, ',');
+				std::getline(iss, sPhoto, ',');
+				std::getline(iss, sDescription, ',');
+				std::getline(iss, bHungry, ',');
+				std::getline(iss, eSatisfaction, ',');
+				std::getline(iss, bBadFood, ',');
+
+				//// Print the parsed values to the console
+				std::cout << "Title: " << sTitle << std::endl;
+				std::cout << "Date: " << sDate << std::endl;
+				std::cout << "Photo: " << sPhoto << std::endl;
+				std::cout << "Description: " << sDescription << std::endl;
+				std::cout << "Hungry: " << bHungry << std::endl;
+				std::cout << "Satisfaction: " << eSatisfaction << std::endl;
+				std::cout << "BadFood: " << bBadFood << std::endl;
+				std::getline(std::cin, input);
+				break;
 				case MenuOption::VIEW_FOOD:
-					std::cout << sNotImplemented << std::endl;
-					std::getline(std::cin, input);
-					break;
+				std::cout << sNotImplemented << std::endl;
+				std::getline(std::cin, input);
+				break;
 				case MenuOption::EXIT:
-					std::cout << "Saliendo..." << std::endl;
-					std::this_thread::sleep_for(1s);
-					return 0;
+				std::cout << "Saliendo..." << std::endl;
+				std::this_thread::sleep_for(1s);
+				return 0;
 				default:
-					std::cout << sInvalidInputMsg << std::endl;
+				std::cout << sInvalidInputMsg << std::endl;
 			}
 		}
 		catch (std::invalid_argument&)
